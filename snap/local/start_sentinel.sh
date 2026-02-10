34f3b888d190182461121f4b42aa406e374b8f76
Smail Kourta <smail.kourta@canonical.com>
2026-02-10 05:54:58 +0000

    add sentinel daemon

---
+++ snap/local/start_sentinel.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# For security measures, daemons should not be run as sudo. Execute valkey as the non-sudo user: snap-daemon.
+exec "${SNAP}"/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}"/usr/bin/valkey-sentinel "${SNAP_DATA}"/etc/charmed-valkey/sentinel.conf
