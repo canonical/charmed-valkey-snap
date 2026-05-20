fa9837e173e2241065f193203d2b6c31bb790085
Mehdi-Bendriss <45567724+Mehdi-Bendriss@users.noreply.github.com>
2026-05-20 18:35:41 +0200

    patch: add script for redis_exporter

--- snap/local/start_redis_exporter.sh
+++ snap/local/start_redis_exporter.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+IS_CLUSTER="$(snapctl get is-cluster)"
+if [ -z "${IS_CLUSTER}" ] || [ "${IS_CLUSTER}" != "true" ]; then
+    IS_CLUSTER="false"
+fi
+snapctl set is-cluster="${IS_CLUSTER}"
+
 # For security measures, daemons should not be run as sudo. Execute valkey as the non-sudo user: snap-daemon.
 exec "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid snap_daemon \
     --regid snap_daemon \
     -- \
-    "${SNAP}"/usr/bin/valkey-server "${SNAP_DATA}"/etc/charmed-valkey/valkey.conf
+    "${SNAP}"/usr/bin/redis_exporter --is-cluster="${IS_CLUSTER}"
