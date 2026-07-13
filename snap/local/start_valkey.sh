52a4315abb15af7c492ddb3180e7d4cd11d7869a
René Radoi <rene.radoi@canonical.com>
2026-07-13 13:54:35 +0200

    [DPE-10645] Fix storage paths (#18)
    According to DA277, the subdirectories within the SNAP directories are
    supposed to be called `valkey` instead of `charmed-valkey`. This PR
    adjusts the paths that are installed.

--- snap/local/start_valkey.sh
+++ snap/local/start_valkey.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # For security measures, daemons should not be run as sudo. Execute valkey as the non-sudo user: snap-daemon.
-exec "${SNAP}"/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}"/usr/bin/valkey-server "${SNAP_DATA}"/etc/charmed-valkey/valkey.conf
+exec "${SNAP}"/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}"/usr/bin/valkey-server "${SNAP_DATA}"/etc/valkey/valkey.conf
